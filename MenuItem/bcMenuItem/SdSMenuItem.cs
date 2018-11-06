0406980e422595768f106d8128eb6e05ff47f369
Rubén Martínez <43479678+rubenmarsim@users.noreply.github.com>
2018-11-06 16:08:56 +0100

    SDSMenuItem

---
+++ MenuItem/bcMenuItem/SdSMenuItem.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bcMenuItem
+{
+    public class SdSMenuItem : ToolStripMenuItem
+    {
+
+
+        public SdSMenuItem()
+        {
+            InitializeComponent();
+        }
+
+
+        #region Classe
+        private string _Classe;
+        public string Classe
+        {
+            get
+            {
+                return _Classe;
+            }
+            set
+            {
+                _Classe = value;
+            }
+        }
+        #endregion
+
+        #region Form
+        private string _Form;
+        public string Form
+        {
+            get
+            {
+                return _Form;
+            }
+            set
+            {
+                _Form = value;
+            }
+        }
+        #endregion
+
+        private void InitializeComponent()
+        {
+            this.Click += new System.EventHandler(this.ObreForm);
+        }
+
+        public void ObreForm(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola");
+        }
+
+
+    }
+}
