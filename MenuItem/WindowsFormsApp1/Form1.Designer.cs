6b8c2549638a62a588b0770d8b7e5ed76ab01478
Rubén Martínez <43479678+rubenmarsim@users.noreply.github.com>
2018-11-06 16:04:16 +0100

    SDSMenuItem

---
+++ MenuItem/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(589, 288);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(105, 190);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(447, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ MenuItem/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int cont = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string _dll;
+
+        public string dll
+        {
+            get { return _dll; }
+            set { _dll = value; }
+        }
+
+
+        private string _hola;
+
+        public string hola
+        {
+            get { return _hola; }
+            set { _hola = value; }
+        }
+
+        private string _culdolla;
+
+        public string culdolla
+        {
+            get { return _culdolla; }
+            set { _culdolla = value; }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+                ConnectionClass.Class1 db;
+
+                db = new ConnectionClass.Class1();
+
+                DataSet dts;
+
+                dts = db.portaTaula("Menu");
+
+                dataGridView1.DataSource = dts.Tables[0];
+
+                foreach (DataRow dr in dts.Tables[0].Rows)
+                {
+                    var fruitToolStripMenuItem = new bcMenuItem.SdSMenuItem();
+
+                    fruitToolStripMenuItem.Text = dr["textmenu"].ToString();
+                    fruitToolStripMenuItem.Classe = dr["DLL"].ToString();
+                    fruitToolStripMenuItem.Form = dr["Form"].ToString();
+                    //fruitToolStripMenuItem.Click += new System.EventHandler();
+                    menuStrip1.Items.Add(fruitToolStripMenuItem);
+                    
+                }
+
+        }
+
+    }
+}
