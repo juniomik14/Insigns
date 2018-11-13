691ee008c50effa135100c8d1ab554e5d628df22
sergio diaz marcos <xexidiaz@hotmail.com>
2018-11-13 19:23:49 +0100

    Update ConnectionClass/ClassDB.cs
    fdfs

--- ConnectionClass/ClassDB.cs
+++ ConnectionClass/ClassDB.cs
@@ -9,105 +9,95 @@ using System.Configuration;
 
 namespace ConnectionClass
 {
-    public class ClassDB { 
+    public class ClassDB
+    {
 
 
         private string _ConnectionString;
         OleDbConnection connexxion;
 
-    public void getConnexionString()
-    {
-        _ConnectionString = ConfigurationManager.ConnectionStrings["BadgesConnectionString"].ConnectionString;
-
-    }
-
-    public void connectBD()
-    {
-        getConnexionString();
-        connexxion = new  OleDbConnection(_ConnectionString);
-        connexxion.Open();
-    }
-
-    public DataSet portaTaula(string nomTaula)
-    {
-
-        connectBD();
-        string query = "SELECT * FROM " + nomTaula;
-        OleDbDataAdapter   adapter = new OleDbDataAdapter(query, _ConnectionString);
-        DataSet dtsCli = new DataSet();
-        adapter.Fill(dtsCli, nomTaula);
-
-        return dtsCli;
-    }
-
-    public void Executa(string consulta)
-    {
-        try
+        public void getConnexionString()
         {
+            _ConnectionString = ConfigurationManager.ConnectionStrings["BadgesConnectionString"].ConnectionString;
+
+        }
+
+        public void connectBD()
+        {
+            getConnexionString();
+            connexxion = new OleDbConnection(_ConnectionString);
+            connexxion.Open();
+        }
+
+        public DataSet portaTaula(string nomTaula)
+        {
+
             connectBD();
-            OleDbCommand comanda = new OleDbCommand(consulta, connexxion);
+            string query = "SELECT * FROM " + nomTaula;
+            OleDbDataAdapter adapter = new OleDbDataAdapter(query, _ConnectionString);
+            DataSet dtsCli = new DataSet();
+            adapter.Fill(dtsCli, nomTaula);
+
+            return dtsCli;
         }
-        catch (OleDbException e)
+
+        public void Executa(string consulta)
         {
+            try
+            {
+                connectBD();
+                OleDbCommand comanda = new OleDbCommand(consulta, connexxion);
+            }
+            catch (OleDbException e)
+            {
+            }
+            finally
+            {
+                connexxion.Close();
+
+            }
+
+
         }
-        finally
+
+        private void portaDadesOLEDB(string nomTaula)
         {
-            connexxion.Close();
-
-        }
-
-
-    }
-
-        private void portaDadesOLEDB(string nomTaula) {
             connectBD();
             string query = "SELECT * FROM " + nomTaula;
             OleDbDataAdapter adapter = new OleDbDataAdapter(query, _ConnectionString);
             DataSet dtsCli = new DataSet();
 
         }
-        //private void bindingDades() {
 
-        //    foreach (txt in )
-        //    {
-        //        if (z is TextBox)
-        //        {
-        //            ((TextBox)z).Text = ((TextBox)z).Name;
-        //        }
-
-
-        //    }
-
-        //}
         public void Actualitzar(DataSet dts, string consulta)
-    {
-        try
         {
-            connectBD();
+            try
+            {
+                connectBD();
                 OleDbDataAdapter adapterDts = new OleDbDataAdapter(consulta, _ConnectionString);
-            adapterDts.Update(dts, consulta);
+                adapterDts.Update(dts, consulta);
+            }
+            catch (OleDbException e)
+            {
+
+            }
+            finally
+            {
+                connexxion.Close();
+
+            }
         }
-        catch (OleDbException e)
+        public DataSet portaPerConsulta(string consultaSql)
         {
 
+            connectBD();
+            OleDbDataAdapter adapter = new OleDbDataAdapter(consultaSql, _ConnectionString);
+            DataSet dtsCli = new DataSet();
+            adapter.Fill(dtsCli, consultaSql);
+
+            return dtsCli;
         }
-        finally
-        {
-            connexxion.Close();
 
-        }
+
     }
-    public DataSet portaPerConsulta(string consultaSql)
-    {
-
-        connectBD();
-        OleDbDataAdapter adapter = new OleDbDataAdapter(consultaSql, _ConnectionString);
-        DataSet dtsCli = new DataSet();
-        adapter.Fill(dtsCli, consultaSql);
-
-        return dtsCli;
-    }
-
-
-}
 }
